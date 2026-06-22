ce7ccb534e8dde041f878e7506af25aa31becd98
Amarachi Crystal Omereife <Mirrorprojectsltd@gmail.com>
2026-06-22 10:10:07 -0400

    Delete secure S3 bucket and access block resources
    Removed S3 bucket and public access block resources.

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,15 +1,3 @@
-resource "aws_s3_bucket" "secure_bucket" {
-  bucket = "my-secure-demo-bucket"
-}
-
-resource "aws_s3_bucket_public_access_block" "secure_access" {
-  bucket                  = aws_s3_bucket.secure_bucket.id
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-}
-
 # 1. Define the Provider
 provider "aws" {
   region = "us-east-1"
